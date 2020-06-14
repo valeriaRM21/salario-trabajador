6ab33389f2053f02c1b5a3ae0be6fb556b740013
valeria Ramirez Marin <62155512+valeriaRM21@users.noreply.github.com>
2020-06-13 20:36:54 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+// salario de trabajador x horas
+$salarioxhoras=20000;
+$horasextras=25000;
+
+
+$salariosemanas=36*$salarioxhoras;
+
+
+echo("el total de horas trabajadas por usted esta semana es de 36 horas");
+echo("<br>");
+echo("señor trabajador su salario de esta semana es ");
+echo("<br>");
+echo($salariosemanas);
+
+
+
+
+
+
+?>
